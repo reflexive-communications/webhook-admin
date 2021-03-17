e49efd8a6c9ec977250b8da98f209cbfbe5f1f94
akosgarai <szlem01@gmail.com>
2021-03-17 15:15:43 +0100

    Generate testcases for config

---
+++ tests/phpunit/CRM/Webhook/ConfigHeadlessTest.php
@@ -0,0 +1,117 @@
+<?php
+
+use CRM_Webhook_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * FIXME - Add test description.
+ *
+ * @group headless
+ */
+class CRM_Webhook_ConfigHeadlessTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+    public function setUpHeadless() {
+        return \Civi\Test::headless()
+            ->installMe(__DIR__)
+            ->apply();
+    }
+
+    public function setUp(): void {
+        parent::setUp();
+    }
+
+    public function tearDown(): void {
+        parent::tearDown();
+    }
+
+    private function isDefaultConfiguration(array $cfg) {
+        self::assertTrue(array_key_exists("sequence", $cfg), "sequence key is missing from the config.");
+        self::assertEquals(1, $cfg["sequence"], "Invalid sequence initial value.");
+        self::assertTrue(array_key_exists("webhooks", $cfg), "webhooks key is missing from the config.");
+        self::assertEquals(1, count($cfg["webhooks"]), "Invalid initial number of webhooks.");
+        self::assertTrue(array_key_exists("name", $cfg["webhooks"][0]), "webhooks[0].name key is missing from the config.");
+        self::assertEquals(CRM_Webhook_Config::DEFAULT_HOOK_NAME, $cfg["webhooks"][0]["name"], "Invalid webhooks[0].name.");
+        self::assertTrue(array_key_exists("label", $cfg["webhooks"][0]), "webhooks[0].label key is missing from the config.");
+        self::assertEquals(CRM_Webhook_Config::DEFAULT_HOOK_LABEL, $cfg["webhooks"][0]["label"], "Invalid webhooks[0].label.");
+        self::assertTrue(array_key_exists("description", $cfg["webhooks"][0]), "webhooks[0].description key is missing from the config.");
+        self::assertEquals(CRM_Webhook_Config::DEFAULT_HOOK_DESC, $cfg["webhooks"][0]["description"], "Invalid webhooks[0].description.");
+        self::assertTrue(array_key_exists("handler", $cfg["webhooks"][0]), "webhooks[0].handler key is missing from the config.");
+        self::assertEquals(CRM_Webhook_Config::DEFAULT_HOOK_HANDLER, $cfg["webhooks"][0]["handler"], "Invalid webhooks[0].handler.");
+        self::assertTrue(array_key_exists("selector", $cfg["webhooks"][0]), "webhooks[0].selector key is missing from the config.");
+        self::assertEquals(CRM_Webhook_Config::DEFAULT_HOOK_SELECTOR, $cfg["webhooks"][0]["selector"], "Invalid webhooks[0].selector.");
+        self::assertTrue(array_key_exists("id", $cfg["webhooks"][0]), "webhooks[0].id key is missing from the config.");
+        self::assertEquals(0, $cfg["webhooks"][0]["id"], "Invalid webhooks[0].id.");
+    }
+    /**
+     * It checks that the create function works well.
+     */
+    public function testCreate() {
+        $config = new CRM_Webhook_Config("webhook_test");
+        self::assertTrue($config->create(), "Create config has to be successful.");
+        $cfg = $config->get();
+        $this->isDefaultConfiguration($cfg);
+    }
+
+    /**
+     * It checks that the remove function works well.
+     */
+    public function testRemove() {
+        $config = new CRM_Webhook_Config("webhook_test");
+        self::assertTrue($config->create(), "Create config has to be successful.");
+        self::assertTrue($config->remove(), "Remove config has to be successful.");
+    }
+
+    /**
+     * It checks that the get function works well.
+     */
+    public function testGet() {
+        $config = new CRM_Webhook_Config("webhook_test");
+        self::assertTrue($config->create(), "Create config has to be successful.");
+        $config = new CRM_Webhook_Config("webhook_test");
+        $cfg = $config->get();
+        $this->isDefaultConfiguration($cfg);
+
+        self::assertTrue($config->remove(), "Remove config has to be successful.");
+        self::expectException(CRM_Core_Exception::class, "Invalid exception class.");
+        self::expectExceptionMessage("webhook_test_configuration config is missing.", "Invalid exception message.");
+        $cfg = $config->get();
+    }
+
+    /**
+     * It checks that the update function works well.
+     */
+    public function testUpdate() {
+        $config = new CRM_Webhook_Config("webhook_test");
+        self::assertTrue($config->create(), "Create config has to be successful.");
+        $cfg = $config->get();
+        $cfg["webhooks"][0]["name"] = "brand new name";
+        self::assertTrue($config->update($cfg), "Update config has to be successful.");
+        $cfgUpdated = $config->get();
+        self::assertEquals($cfg, $cfgUpdated, "Invalid updated configuration.");
+    }
+
+    /**
+     * It checks that the load function works well.
+     */
+    public function testLoad() {
+        $config = new CRM_Webhook_Config("webhook_test");
+        self::assertTrue($config->create(), "Create config has to be successful.");
+        $cfg = $config->get();
+        $cfg["webhooks"][0]["name"] = "brand new name";
+        self::assertTrue($config->update($cfg), "Update config has to be successful.");
+        $cfgUpdated = $config->get();
+        self::assertEquals($cfg, $cfgUpdated, "Invalid updated configuration.");
+        $otherConfig = new CRM_Webhook_Config("webhook_test");
+        self::assertEmpty($otherConfig->load(), "Load result supposed to be empty.");
+
+        $cfgLoaded = $otherConfig->get();
+        self::assertEquals($cfg, $cfgLoaded, "Invalid loaded configuration.");
+
+        $missingConfig = new CRM_Webhook_Config("civalpa_test_missing_config");
+        self::expectException(CRM_Core_Exception::class, "Invalid exception class.");
+        self::expectExceptionMessage("civalpa_test_missing_config_configuration config invalid.", "Invalid exception message.");
+        self::assertEmpty($missingConfig->load(), "Load result supposed to be empty.");
+    }
+}
---
+++ tests/phpunit/CRM/Webhook/ConfigTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This is a generic test class for the extension (implemented with PHPUnit).
+ */
+class CRM_Webhook_ConfigTest extends \PHPUnit\Framework\TestCase {
+
+    /**
+     * The setup() method is executed before the test is executed (optional).
+     */
+    public function setUp(): void {
+        parent::setUp();
+    }
+
+    /**
+     * The tearDown() method is executed after the test was executed (optional)
+     * This can be used for cleanup.
+     */
+    public function tearDown(): void {
+        parent::tearDown();
+    }
+
+    /**
+     * Default configuration test case.
+     *
+     */
+    public function testConfigAfterConstructor() {
+        $config = new CRM_Webhook_Config("webhook_test");
+        $cfg = $config->get();
+        self::assertTrue(array_key_exists("sequence", $cfg), "sequence key is missing from the config.");
+        self::assertEquals(1, $cfg["sequence"], "Invalid sequence initial value.");
+        self::assertTrue(array_key_exists("webhooks", $cfg), "webhooks key is missing from the config.");
+        self::assertEquals(1, count($cfg["webhooks"]), "Invalid initial number of webhooks.");
+        self::assertTrue(array_key_exists("name", $cfg["webhooks"][0]), "webhooks[0].name key is missing from the config.");
+        self::assertEquals($config::DEFAULT_HOOK_NAME, $cfg["webhooks"][0]["name"], "Invalid webhooks[0].name.");
+        self::assertTrue(array_key_exists("label", $cfg["webhooks"][0]), "webhooks[0].label key is missing from the config.");
+        self::assertEquals($config::DEFAULT_HOOK_LABEL, $cfg["webhooks"][0]["label"], "Invalid webhooks[0].label.");
+        self::assertTrue(array_key_exists("description", $cfg["webhooks"][0]), "webhooks[0].description key is missing from the config.");
+        self::assertEquals($config::DEFAULT_HOOK_DESC, $cfg["webhooks"][0]["description"], "Invalid webhooks[0].description.");
+        self::assertTrue(array_key_exists("handler", $cfg["webhooks"][0]), "webhooks[0].handler key is missing from the config.");
+        self::assertEquals($config::DEFAULT_HOOK_HANDLER, $cfg["webhooks"][0]["handler"], "Invalid webhooks[0].handler.");
+        self::assertTrue(array_key_exists("selector", $cfg["webhooks"][0]), "webhooks[0].selector key is missing from the config.");
+        self::assertEquals($config::DEFAULT_HOOK_SELECTOR, $cfg["webhooks"][0]["selector"], "Invalid webhooks[0].selector.");
+        self::assertTrue(array_key_exists("id", $cfg["webhooks"][0]), "webhooks[0].id key is missing from the config.");
+        self::assertEquals(0, $cfg["webhooks"][0]["id"], "Invalid webhooks[0].id.");
+    }
+}
