1b78e77f5b21d10a95a2d7ffe137a0954356392b
akosgarai <szlem01@gmail.com>
2021-03-18 16:53:45 +0100

    Input validation - selector duplication

--- CRM/Webhook/Form/WebhookForm.php
+++ CRM/Webhook/Form/WebhookForm.php
@@ -36,6 +36,49 @@ class CRM_Webhook_Form_WebhookForm extends CRM_Webhook_Form_WebhookBase {
         $this->setTitle(ts('Webhook Form'));
     }
 
+    /**
+     * Add form validation rules
+     */
+    public function addRules() {
+        $this->addFormRule(
+            ["CRM_Webhook_Form_WebhookForm", "validateSelector"],
+            ['config' => $this->config,]
+        );
+    }
+
+    /**
+     * Validate selector
+     *
+     * @param array $values Submitted values
+     * @param array $files Uploaded files
+     * @param array $options Options to pass to function
+     *
+     * @return array|bool
+     */
+    protected function validateSelector($values, $files, $options)
+    {
+        $errors = [];
+        // Update configuration to latest values
+        $options["config"]->load();
+        $config = $options["config"]->get();
+
+        // Loop through existing webhooks for duplication checking
+        foreach ($config["webhooks"] as $hook) {
+
+            // Handle duplication
+            if ($hook["selector"] == $values["selector"]) {
+                $errors["selector"] = ts(
+                    "The selector '%1' already set for the '%2' webhook.",
+                    ["1" => $values["selector"], "2" => $hook["name"],]
+                );
+
+                return $errors;
+            }
+        }
+
+        return true;
+    }
+
     public function postProcess() {
         parent::postProcess();
     }
