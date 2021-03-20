80e9c3dbc31970b8b8d23f1bdea7ffc37acfa7f1
akosgarai <szlem01@gmail.com>
2021-03-20 23:25:49 +0100

    Add logger handler

---
+++ CRM/Webhook/Handler/Logger.php
@@ -0,0 +1,37 @@
+<?php
+
+use CRM_Webhook_ExtensionUtil as E;
+
+/**
+ * Webhook Handler for Logger
+ */
+class CRM_Webhook_Handler_Logger extends CRM_Webhook_Handler_Base
+{
+    /**
+     * Authenticate request
+     * Logger authenticates everything.
+     *
+     * @return bool
+     */
+    protected function authenticate(): bool {
+        return true;
+    }
+
+    /**
+     * Validate request data
+     *
+     * @return bool
+     */
+    protected function validate(): bool {
+        return true;
+    }
+
+    /**
+     * Handle request
+     */
+    public function handle() {
+        parent::handle();
+        $config = new CRM_Webhook_Config(E::LONG_NAME);
+        $config->insertLog($this->data);
+    }
+}
