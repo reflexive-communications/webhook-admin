a3ba86aada01c351562e8c5283d3d34826d8f0e5
akosgarai <szlem01@gmail.com>
2021-04-02 12:11:14 +0200

    Extend config from rc-base

--- CRM/Webhook/Config.php
+++ CRM/Webhook/Config.php
@@ -1,26 +1,12 @@
 <?php
 
-class CRM_Webhook_Config
+class CRM_Webhook_Config extends CRM_RcBase_Config
 {
     const DEFAULT_HOOK_NAME = "Logger webhook";
     const DEFAULT_HOOK_DESC = "This webhook could be used for testing purposes. It logs the received data.";
     const DEFAULT_HOOK_HANDLER = "CRM_Webhook_Handler_Logger";
     const DEFAULT_HOOK_SELECTOR = "logger-hook";
     const DEFAULT_HOOK_PROCESSOR = "CRM_Webhook_Processor_Dummy";
-
-    private $configName;
-    private $configuration;
-
-    /**
-     * CRM_Webhook_Config constructor.
-     *
-     * @param string $extensionName prefix for db.
-     */
-    public function __construct(string $extensionName)
-    {
-        $this->configName = $extensionName."_configuration";
-        $this->configuration = $this->defaultConfiguration();
-    }
 
     /**
      * Provides a default configuration object.
@@ -44,89 +30,6 @@ class CRM_Webhook_Config
                 ],
             ],
         ];
-    }
-
-    /**
-     * Creates configuration table in db.
-     *
-     * @return bool the status of the db write process.
-     */
-    public function create(): bool
-    {
-        Civi::settings()->add([$this->configName => $this->configuration]);
-        // check the save process with loading the saved content and compare
-        // it with the current configuration
-        $saved = Civi::settings()->get($this->configName);
-        return $saved === $this->configuration;
-    }
-
-    /**
-     * Removes configuration table from db.
-     *
-     * @return bool the status of the deletion process.
-     */
-    public function remove(): bool
-    {
-        Civi::settings()->revert($this->configName);
-        // check the deletion process with loading the saved content and compare
-        // it with null.
-        $saved = Civi::settings()->get($this->configName);
-        if (is_null($saved)) {
-            // cleanup the class config
-            $this->configuration = null;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Loads configuration from db.
-     *
-     * @throws CRM_Core_Exception.
-     */
-    public function load(): void
-    {
-        $conf = Civi::settings()->get($this->configName);
-        // if not loaded well, it throws exception.
-        if (is_null($conf) || !is_array($conf)) {
-            throw new CRM_Core_Exception($this->configName." config invalid.");
-        }
-        $this->configuration = $conf;
-    }
-
-    /**
-     * Updates the configuration in db.
-     *
-     * @param array $newConfig the updated config to save
-     *
-     * @return bool the status of the update process.
-     */
-    public function update(array $newConfig): bool
-    {
-        Civi::settings()->set($this->configName, $newConfig);
-        // check the save process with loading the saved content and compare
-        // it with the newConfig configuration
-        $saved = Civi::settings()->get($this->configName);
-        if ($saved === $newConfig) {
-            $this->configuration = $newConfig;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Returns the configuration.
-     *
-     * @return array the configuration.
-     *
-     * @throws CRM_Core_Exception.
-     */
-    public function get(): array
-    {
-        if (is_null($this->configuration)) {
-            throw new CRM_Core_Exception($this->configName." config is missing.");
-        }
-        return $this->configuration;
     }
 
     /**
