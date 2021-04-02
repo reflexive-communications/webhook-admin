dc8d734afd2d06e8a9b05a7daf2afcf6a5612748
akosgarai <szlem01@gmail.com>
2021-04-02 16:30:47 +0200

    Update upgrader tests, implement test for upgrade

--- tests/phpunit/CRM/Webhook/UpgraderHeadlessTest.php
+++ tests/phpunit/CRM/Webhook/UpgraderHeadlessTest.php
@@ -10,7 +10,7 @@ use Civi\Test\TransactionalInterface;
  *
  * @group headless
  */
-class CRM_Webhook_UpgraderHeadlessTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface
+class CRM_Webhook_UpgraderHeadlessTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface
 {
 
     public function setUpHeadless()
@@ -31,6 +31,18 @@ class CRM_Webhook_UpgraderHeadlessTest extends \PHPUnit\Framework\TestCase imple
     }
 
     /**
+     * Create a clean DB before running tests
+     *
+     * @throws CRM_Extension_Exception_ParseException
+     */
+    public static function tearDownAfterClass(): void
+    {
+        \Civi\Test::headless()
+            ->uninstallMe(__DIR__)
+            ->apply(true);
+    }
+
+    /**
      * Test the install process.
      */
     public function testInstall()
@@ -39,7 +51,7 @@ class CRM_Webhook_UpgraderHeadlessTest extends \PHPUnit\Framework\TestCase imple
         try {
             $this->assertEmpty($installer->install());
         } catch (Exception $e) {
-            $this->fail("Should not throw exception.");
+            $this->fail("Should not throw exception. ".$e->getMessage());
         }
     }
 
@@ -53,7 +65,59 @@ class CRM_Webhook_UpgraderHeadlessTest extends \PHPUnit\Framework\TestCase imple
         try {
             $this->assertEmpty($installer->uninstall());
         } catch (Exception $e) {
-            $this->fail("Should not throw exception.");
+            $this->fail("Should not throw exception. ".$e->getMessage());
         }
+    }
+
+    /**
+     * Test the upgrade process.
+     */
+    public function testUpgrade_5000FreshInstall()
+    {
+        $installer = new CRM_Webhook_Upgrader("webhook_test", ".");
+        $this->assertEmpty($installer->install());
+        try {
+            $this->assertTrue($installer->upgrade_5000());
+        } catch (Exception $e) {
+            $this->fail("Should not throw exception. ".$e->getMessage());
+        }
+    }
+    public function testUpgrade_5000HasPreviousInstall()
+    {
+        $origConfig = [
+            "sequence" => 2,
+            "logs" => [],
+            "webhooks" => [
+                0 => [
+                    "name" => "orig 1",
+                    "description" => "orig 1 desc",
+                    "handler" => "orig-handler-class-1",
+                    "selector" => "orig-selector-1",
+                    "processor" => "orig-processor-class-1",
+                    "options" => [],
+                    "id" => 0,
+                ],
+                1 => [
+                    "name" => "orig 2",
+                    "description" => "orig 2 desc",
+                    "handler" => "orig-handler-class-2",
+                    "selector" => "orig-selector-2",
+                    "processor" => "orig-processor-class-2",
+                    "options" => [],
+                    "id" => 1,
+                ],
+            ],
+        ];
+        Civi::settings()->add(["webhook_test_configuration" => $origConfig]);
+        $installer = new CRM_Webhook_Upgrader("webhook_test", ".");
+        $this->assertEmpty($installer->install());
+        try {
+            $this->assertTrue($installer->upgrade_5000());
+        } catch (Exception $e) {
+            $this->fail("Should not throw exception. ".$e->getMessage());
+        }
+        $newConfig = Civi::settings()->get("webhook_test_config");
+        $this->assertEquals($origConfig, $newConfig, "Config has to be the same after the migration.");
+        $this->assertNull(Civi::settings()->get("webhook_test_configuration"), "The orig config has to be removed.");
     }
 }
