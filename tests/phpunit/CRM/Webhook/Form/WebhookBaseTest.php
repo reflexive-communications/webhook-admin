21b9d767b42b3bf5e48d33486b90e0ad7053f4ef
akosgarai <szlem01@gmail.com>
2021-04-20 16:00:43 +0200

    Fix test-cases

--- tests/phpunit/CRM/Webhook/Form/WebhookBaseTest.php
+++ tests/phpunit/CRM/Webhook/Form/WebhookBaseTest.php
@@ -37,7 +37,7 @@ class CRM_Webhook_Form_WebhookBaseTest extends CRM_Webhook_Form_TestBase
         $config = new CRM_Webhook_Config(E::LONG_NAME);
         $config->remove();
         self::expectException(CRM_Core_Exception::class);
-        self::expectExceptionMessage(E::SHORT_NAME."_config config invalid.");
+        self::expectExceptionMessage(E::LONG_NAME."_config config invalid.");
         self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
     }
 }
