5e469402ccad5e9d25d7a3e0968379171e0f82db
akosgarai <szlem01@gmail.com>
2021-08-20 20:08:10 +0200

    Generate Webhook entity
    The output of the 'civix generate:entity Webhook' command.

---
+++ Civi/Api4/Webhook.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * Webhook entity.
+ *
+ * Provided by the CiviCRM webhooks extension.
+ *
+ * @package Civi\Api4
+ */
+class Webhook extends Generic\DAOEntity {
+
+}
