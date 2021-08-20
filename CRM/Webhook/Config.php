4313618efaaabb7dd7696ad2e5e9160467348f6f
akosgarai <szlem01@gmail.com>
2021-08-20 21:10:19 +0200

    Cleanup from the Config class

--- CRM/Webhook/Config.php
+++ CRM/Webhook/Config.php
@@ -2,11 +2,6 @@
 
 class CRM_Webhook_Config extends CRM_RcBase_Config
 {
-    const DEFAULT_HOOK_NAME = "Logger webhook";
-    const DEFAULT_HOOK_DESC = "This webhook could be used for testing purposes. It logs the received data.";
-    const DEFAULT_HOOK_HANDLER = "CRM_Webhook_Handler_Logger";
-    const DEFAULT_HOOK_SELECTOR = "logger-hook";
-    const DEFAULT_HOOK_PROCESSOR = "CRM_Webhook_Processor_Dummy";
 
     /**
      * Provides a default configuration object.
@@ -16,91 +11,8 @@ class CRM_Webhook_Config extends CRM_RcBase_Config
     public function defaultConfiguration(): array
     {
         return [
-            "sequence" => 1,
             "logs" => [],
-            "webhooks" => [
-                0 => [
-                    "name" => self::DEFAULT_HOOK_NAME,
-                    "description" => self::DEFAULT_HOOK_DESC,
-                    "handler" => self::DEFAULT_HOOK_HANDLER,
-                    "selector" => self::DEFAULT_HOOK_SELECTOR,
-                    "processor" => self::DEFAULT_HOOK_PROCESSOR,
-                    "options" => [],
-                    "id" => 0,
-                ],
-            ],
         ];
-    }
-
-    /**
-     * Inserts a new webhook.
-     *
-     * @param array $webhook the data to save
-     *
-     * @return bool the status of the insert process.
-     *
-     * @throws CRM_Core_Exception.
-     */
-    public function addWebhook(array $webhook): bool
-    {
-        // load latest config
-        parent::load();
-        $configuration = parent::get();
-        // duplication check - in case of duplication throws exception
-        foreach ($configuration["webhooks"] as $hook) {
-            if ($hook["selector"] == $webhook["selector"]) {
-                throw new CRM_Core_Exception($webhook["selector"]." selector is duplicated.");
-            }
-        }
-        // get the id and increment the global one
-        $id = $configuration["sequence"];
-        $configuration["sequence"] += 1;
-        // save hook under the id.
-        $webhook["id"] = $id;
-        $configuration["webhooks"][$id] = $webhook;
-        return parent::update($configuration);
-    }
-
-    /**
-     * Updates an existing webhook.
-     *
-     * @param array $webhook the data to save
-     *
-     * @return bool the status of the update process.
-     *
-     * @throws CRM_Core_Exception.
-     */
-    public function updateWebhook(array $webhook): bool
-    {
-        // load latest config
-        parent::load();
-        $configuration = parent::get();
-        // duplication check - in case of duplication throws exception
-        foreach ($configuration["webhooks"] as $hook) {
-            if ($hook["selector"] == $webhook["selector"] && $webhook["id"] != $hook["id"]) {
-                throw new CRM_Core_Exception($webhook["selector"]." selector is duplicated.");
-            }
-        }
-        $configuration["webhooks"][$webhook["id"]] = $webhook;
-        return parent::update($configuration);
-    }
-
-    /**
-     * Deletes an existing webhook.
-     *
-     * @param int $webhook the id that we want to delete.
-     *
-     * @return bool the status of the deletion process.
-     *
-     * @throws CRM_Core_Exception.
-     */
-    public function deleteWebhook(int $webhook): bool
-    {
-        // load latest config
-        parent::load();
-        $configuration = parent::get();
-        unset($configuration["webhooks"][$webhook]);
-        return parent::update($configuration);
     }
 
     /**
