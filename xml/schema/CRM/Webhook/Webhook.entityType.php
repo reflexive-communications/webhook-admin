dcb614e13e2e031931dd2a8d46594eb36a720484
Sandor Semsey <sandor@es-progress.hu>
2023-03-21 22:08:49 +0100

    remove boilerplate

--- xml/schema/CRM/Webhook/Webhook.entityType.php
+++ xml/schema/CRM/Webhook/Webhook.entityType.php
@@ -1,6 +1,5 @@
 <?php
-// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
-// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+
 return [
     [
         'name' => 'Webhook',
